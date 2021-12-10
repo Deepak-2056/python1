4b6ee2bda61e11ad974f55e89ec1a8b84613cbe4
deepak-2056 <mrjagridon85@gmail.com>
2021-12-10 22:27:12 +0545

    letter compose

---
+++ letter.py
@@ -0,0 +1,4 @@
+letter = "dear deepak , your are good learner , thanks!"
+print(letter)
+change = "dear\n\t deepak , your are good learner  \n i hope you will learn coding\n queckly with great hope, \n thanks!"
+print(change)
